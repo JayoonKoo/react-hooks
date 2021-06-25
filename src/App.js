3875bc83fbe6d918b2f6b084e8c3e9b9281c26d8
JayoonKoo <rnwkdbs12@gmail.com>
2021-06-25 20:57:29 +0900

    useEffect를 사용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
   const [item, setItem] = useState(1);
@@ -51,4 +51,85 @@ const AppInput = () => {
   );
 };
 
-export default AppInput;
+const content = [
+	{
+		tab: "Section 1",
+		content: "I'm the content of the Section 1"
+	},
+	{
+		tab: "Section 2",
+		content: "I'm the content of the Section 2"
+	},
+]
+
+const useTabs = (initailValue, allTabs) => {
+	const [currentIndex, setCurrentIndex] = useState(initailValue);
+	if (!allTabs || !Array.isArray(allTabs)) {
+		return;
+	}
+	return {
+		currentItem: allTabs[currentIndex],
+		changeItem: setCurrentIndex,
+	};
+}
+
+const AppTab = () => {
+	const {currentItem, changeItem} = useTabs(1, content);
+	return (
+		<>
+		<div>
+			{content.map((section, index) => <button onClick={() => changeItem(index) }>{section.tab}</button>)}
+		</div>
+		<div>
+			{currentItem.content}
+		</div>
+		</>
+	)
+}
+
+const useTitle = (initailTitle) => {
+	const [title, setTitle] = useState(initailTitle);
+	const updateTitle = () => {
+		const currentTitle = document.querySelector("title");
+		currentTitle.innerText = title;
+	}
+	useEffect(updateTitle, [title]);
+	return setTitle;
+}
+
+const AppTitle = () => {
+	const titleUpdater = useTitle("Loding...");
+	setTimeout(() => titleUpdater("updated"), 3000);
+	return (
+		<div>
+			Hello
+		</div>
+	)
+}
+
+const useClick = onClick => {
+	const element = useRef();
+	useEffect(() => {
+		if (element.current) {
+			element.current.addEventListener('click', onClick);
+		}
+		return () => {
+			if (element.current) {
+				element.current.removeEventListner('click', onClick);
+			}
+		}
+	}, [])
+	return element;
+}
+
+const AppClick = () => {
+	const sayHello = () => console.log("say hello");
+	const title = useClick(sayHello);
+	return (
+		<div>
+			<h1 ref={title}>Title</h1>
+		</div>
+	)
+}
+
+export default AppClick;
